5d204d69622d99813610d0e9aaf3249d10f8d336
Qian <pandachain731@gmail.com>
2016-11-17 10:23:37 -0800

    changed the exporter to make sure no top tweets is set to True

--- Exporter-project.py
+++ Exporter-project.py
@@ -5,37 +5,33 @@ import sys, getopt, got, datetime, codecs
 
 def main(keyword, start, end, max, filename):
 
-    try:
-        tweetCriteria = got.manager.TweetCriteria()
+    tweetCriteria = got.manager.TweetCriteria()
 
-        tweetCriteria.since = start
-        tweetCriteria.until = end
-        tweetCriteria.querySearch = keyword
-        tweetCriteria.maxTweets = int(max)
+    tweetCriteria.since = start
+    tweetCriteria.until = end
+    tweetCriteria.querySearch = keyword
+    tweetCriteria.maxTweets = int(max)
+    tweetCriteria.topTweets = False
 
-        exportedFileName = "output_" + filename + ".csv"
+    exportedFileName = "output_" + filename + ".csv"
 
-        outputFile = codecs.open(exportedFileName, "w+", "utf-8")
+    outputFile = codecs.open(exportedFileName, "w+", "utf-8")
 
-        outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+    outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
 
-        print 'Searching...\n'
+    print 'Searching...\n'
 
-        def receiveBuffer(tweets):
-            for t in tweets:
-                outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (
-                    t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions,
-                    t.hashtags, t.id, t.permalink)))
-            outputFile.flush();
-            print 'More %d saved on file...\n' % len(tweets)
+    def receiveBuffer(tweets):
+        for t in tweets:
+            outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (
+                t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions,
+                t.hashtags, t.id, t.permalink)))
+        outputFile.flush()
+        print 'More %d saved on file...\n' % len(tweets)
 
-        got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+    got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
 
-    except arg:
-        print 'Arguments parser error, try -h' + arg
-    finally:
-        outputFile.close()
-        print 'Done. Output file generated.'
+    print 'Done. Output file generated.'
 
 
 main("#anger OR #happy OR #surprise", "2015-09-10", "2015-09-15", 10, "happy")
